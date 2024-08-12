aeef39be133e07bfc83f7ef4d9fca1b6b033e922
JA54312 <ija54312@gmail.com>
2024-08-12 17:05:24 -0600

    MenuModal

---
+++ src/components/shared/Header/components/Menu/components/ModalMenu.tsx
@@ -0,0 +1,72 @@
+"use client"
+//hooks
+import { useState } from 'react';
+//components
+import Image from 'next/image';
+import Link from 'next/link';
+import classNames from 'classnames/bind';
+//styles
+import styles from './ModalMenu.module.sass'
+
+const urlArrow = '/images/CaretDown.svg'
+
+export const ModalMenu = ({ isOpenModal, closeModal }) => {
+
+    const gotoRoute = () => {
+        closeModal()
+    }
+
+    const cx = classNames.bind(styles);
+
+    const modalStyles = cx('visible', {
+        'modal': isOpenModal,
+    });
+
+
+    return (
+        <div className={modalStyles}>
+            <div className={styles.modal_content}>
+                <div className={styles.containerClose} onClick={gotoRoute}>
+                    <span className={styles.close} >&times;</span>
+                    <span className={styles.text}>Cerrar</span>
+                </div>
+                <div className={styles.containerTitles}>
+                    <div className={styles.ContainerSimpleTitle}>
+                        <Link href='#'>
+                            <span>Servicios</span>
+                        </Link>
+                    </div>
+                    <div className={styles.ContainerSimpleTitle}>
+                        <Link href='#'>
+                            <span>Unidades</span>
+                        </Link>
+                    </div>
+                    <div className={styles.ContainerSimpleTitle}>
+                        <Link href='#'>
+                            <span>¿Porque TMH?</span>
+                        </Link>
+                    </div>
+                    <div className={styles.ContainerSimpleTitle}>
+                        <Link href='#'>
+                            <span>Sobre nosotros</span>
+                        </Link>
+                    </div>
+                </div>
+                <div className={styles.containerPhone}>
+                    <div className={styles.containerImg}>
+                        <Image
+                            src="/icons/Phone.png"
+                            alt="Imagen Telefono"
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                    <div className={styles.containerText}>
+                        <span>Llama gratis</span>
+                        <span>(55) 5510791481</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
