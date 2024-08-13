5ef52baf0b5eb2d0c0a42a36b5a90be1e639ecfd
JA54312 <ija54312@gmail.com>
2024-08-12 20:47:47 -0600

    add link WhatsApp

--- src/components/home/ContactHome/ContactHome.tsx
+++ src/components/home/ContactHome/ContactHome.tsx
@@ -1,5 +1,6 @@
 //components
 import Image from 'next/image'
+import Link from 'next/link'
 //style
 import styles from './ContactHome.module.sass'
 
@@ -36,7 +37,9 @@ export const ContactHome = () => {
                     </div>
                     <div className={styles.containerButtons}>
                         <div className={styles.button}>
-                            <span>Contactar por Whatsapp</span>
+                            <Link href="https://api.whatsapp.com/send?phone=5215510791481&text=%C2%A1Hola%2C%20TMH%20Log%C3%ADstica!%20%F0%9F%8C%9F%0A%0AEstoy%20listo%20para%20optimizar%20mi%20log%C3%ADstica%20y%20almacenaje.%20%C2%BFPodr%C3%ADan%20enviarme%20detalles%20sobre%20sus%20servicios%3F%20%F0%9F%93%A6%F0%9F%9A%9B" target='_blank'>
+                                <span>Contactar por Whatsapp</span>
+                            </Link>
                         </div>
                         <div className={styles.secondButton}>
                             <span>Solicitar una cotización</span>
