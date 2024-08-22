8253691b0a85a32813be555520a7d3fc2a666769
JA54312 <ija54312@gmail.com>
2024-08-21 19:35:08 -0600

    header desktop fixes

--- src/components/shared/Header/components/Logo/Logo.tsx
+++ src/components/shared/Header/components/Logo/Logo.tsx
@@ -1,17 +1,20 @@
 //components
 import Image from 'next/image';
+import Link from 'next/link';
 //styles
 import styles from './Logo.module.sass'
 
 export const Logo = () => {
     return (
         <div className={styles.LogoContainer}>
-            <Image
-                src="/icons/LogoTMH.png"
-                alt="LogoTMH"
-                width={114}
-                height={45}
-            />
+            <Link href="/">
+                <Image
+                    src="/icons/LogoTMH.png"
+                    alt="LogoTMH"
+                    width={114}
+                    height={45}
+                />
+            </Link>
         </div>
     )
 }
